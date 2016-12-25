abab68b7fe5d9c084c7fe4452221d7c4e6304f4b
Glenn Basgaard <glenn_basgaard@yahoo.com>
2016-12-25 18:19:49 -0500

    add routing to index.js and NotFound component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,12 +1,24 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
-import './css/style.css';
+import {BrowserRouter, Match, Miss } from 'react-router';
 
+import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<StorePicker/>, document.querySelector('#main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match exactly pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
 
-render(<App />, document.querySelector('#main'));
+render(<Root />, document.querySelector('#main'));
 
