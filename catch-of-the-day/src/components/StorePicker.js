309607c1f12c17bcf697b3d4861f1a7584fabc5e
Glenn Basgaard <glenn_basgaard@yahoo.com>
2016-12-25 18:29:36 -0500

    add/import helper functions

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
   render() {
@@ -7,7 +8,7 @@ class StorePicker extends React.Component {
       <form className="store-selector">
         {/* This is how you write a comment in React JS. Make sure to put them inside the actual element you're returning*/}
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store</button>
       </form>
     )
