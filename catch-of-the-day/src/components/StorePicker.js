6e509d11cfb3146b22f308be8a746099e1233c60
Glenn Basgaard <glenn_basgaard@yahoo.com>
2016-12-25 15:38:24 -0500

    add jsx form inside React component

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>;
+    // You can comment normally like this outside of JSX
+    return (
+      <form className="store-selector">
+        {/* This is how you write a comment in React JS. Make sure to put them inside the actual element you're returning*/}
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+      );
   }
 }
 
