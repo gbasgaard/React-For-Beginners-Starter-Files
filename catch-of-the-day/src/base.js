cc6108145c80f4165854d79a18817ad6612d4d77
Glenn Basgaard <glenn_basgaard@yahoo.com>
2017-01-02 14:39:48 -0500

    sync Firebase with state for persistence

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyA_jyTU9uc1g40zavmuRwN3xd-LN2Lsv7c",
+  authDomain: "catch-of-the-day-gbasgaard.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-gbasgaard.firebaseio.com"
+});
+
+export default base;
